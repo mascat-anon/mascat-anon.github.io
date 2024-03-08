69c4009dbbbaab4352c90fe17e2d3fc98986daeb
mascat-anon <158069224+mascat-anon@users.noreply.github.com>
2024-03-08 15:33:10 +0900

    Add files via upload

---
+++ KBS/sw.js
@@ -0,0 +1,27 @@
+// Cache name
+const CACHE_NAME = 'Bus Information Board System-caches-v3.70';
+// Cache targets
+const urlsToCache = [
+  './',
+  './common/KBS-Disp-JP.woff"',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
