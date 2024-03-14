03204115a506a024d06bcbf1173420682865311b
mascat-anon <158069224+mascat-anon@users.noreply.github.com>
2024-03-15 01:33:47 +0900

    Add files via upload

---
+++ map/sw.js
@@ -0,0 +1,26 @@
+// Cache name
+const CACHE_NAME = 'caches-v1.0';
+// Cache targets
+const urlsToCache = [
+  './',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
