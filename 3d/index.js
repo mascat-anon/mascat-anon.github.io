948f9b14622c029fd9b8b43f57c02e87ab613389
mascat-anon <158069224+mascat-anon@users.noreply.github.com>
2024-12-25 03:03:08 +0900

    Add files via upload

--- 3d/index.js
+++ 3d/index.js
@@ -4,73 +4,7 @@ function init() {
     // 親要素の取得
     const parentElement = document.querySelector('#mainBoard'); // 親要素を指定
     const width = parentElement.clientWidth;
-    const height = parentElement.window.addEventListener("DOMContentLoaded", init);
-window.addEventListener('resize', init);
-function init() {
-    // 親要素の取得
-    const parentElement = document.querySelector('#mainBoard'); // 親要素を指定
-    const width = parentElement.clientWidth;
-    const height = parentElement.innerHeight;
-
-    // レンダラーを作成
-    const canvasElement = document.querySelector('#myCanvas');
-    const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        canvas: canvasElement,
-    });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(width, height);
-
-    // シーンを作成
-    const scene = new THREE.Scene();
-
-    // カメラを作成
-    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-    camera.position.set(0, 0, 100);
-
-    // カメラコントローラーを作成
-    const controls = new THREE.OrbitControls(camera, canvasElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.5;
-
-
-    // カメラの方向に追従するライト
-    const cameraLight = new THREE.DirectionalLight(0xffffff); // カメラに追従するライト
-    cameraLight.intensity = 0.75;
-    scene.add(cameraLight);
-
-
-    // 3Dモデルの読み込み
-    const objLoader = new THREE.OBJLoader();
-    objLoader.load(
-        './models/mo.obj',
-        function (obj) {
-            scene.add(obj);
-            obj.position.x = 0;
-            obj.position.y = 0;
-        },
-    );
-
-    tick();
-
-    function tick() {
-        // カメラの方向を取得
-        const cameraDirection = new THREE.Vector3();
-        camera.getWorldDirection(cameraDirection);
-
-        // カメラの位置と方向を基準にライトを設定
-        cameraLight.position.copy(camera.position); // カメラ位置にライトを配置
-        cameraLight.target.position.copy(camera.position.clone().add(cameraDirection));
-        cameraLight.target.updateMatrixWorld();
-
-        // コントローラーの更新
-        controls.update();
-
-        renderer.render(scene, camera); // レンダリング
-        requestAnimationFrame(tick);
-    }
-}
-;
+    const height = parentElement.clientHeight;
 
     // レンダラーを作成
     const canvasElement = document.querySelector('#myCanvas');
